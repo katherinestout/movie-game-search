6d0891d1b24dd3edcd952f772878fa20fd54e6ee
nchenari <nimachenari@gmail.com>
2018-06-14 14:12:49 -0400

    significant restructuring to app folder contents and combining into one search route in apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,91 +1,58 @@
+var axios = require("axios");
 var request = require("request");
 var igdb = require("igdb-api-node").default;
 
-// keys
+// api keys
 const OMDB_KEY = "879503ae";
 const IGDB_KEY = "47a0d2b184712d0c3ae138e13c049062";
 
 // routing
 module.exports = function(app) {
-    
-    // get movies based on games
-    app.get("/api/movies/:gameterm", function(req, res) {
+
+    app.get("/api/search/:searchterm", function(req, res) {
+        // *** SEARCH TERM MUST CONTAIN "-" BETWEEN WORDS (ex: "star-wars")
+        var searchTerm = req.params.searchterm;
+        console.log("search term: " + searchTerm);
+
+        // perfor multiple concurrent requests using axios
+        axios.all([getMovies(), getGames()])
+            .then(axios.spread(function (movieData, gameData) {
+                // return resulting data
+                console.log(movieData);
+                console.log(gameData);
+            }));
+
+        function getMovies() {
+            var queryUrl = "http://www.omdbapi.com/?s=" + searchTerm + "&y=&plot=short&apikey=" + OMDB_KEY;
+            console.log("omdb query url: " + queryUrl);
+            request(queryUrl, function(error, response, body) {
+                // If the request is successful
+                if (!error && response.statusCode === 200) {
+                    console.log(body);
+                    return res.json(JSON.parse(body));
+                }
+            });
+        }
         
-        // **** SEARCH URL TERM MUST CONTAIN "-" BETWEEN WORDS (EX: star-wars)
-        var gameTerm = req.params.gameterm;
-        console.log("Game Search Term: " + gameTerm);
+        function getGames() {
+            const client = igdb(IGDB_KEY);
 
-        // Run a request to the IGDB API with the title specified
-        const client = igdb(IGDB_KEY);
-
-        client.games({
-            fields: '*',
-            limit: 10,
-            offset: 0,
-            order: 'popularity:desc',
-            search: gameTerm
-        }).then(response => {
-            // response.body contains the parsed JSON response to this query
-            console.log(response.body);
-            
-            return res.json(response.body);
-
-        }).catch(error => {
-            throw error;
-        });
-
-    });
-
-    // get games based on movies
-    app.get("/api/games/:movieterm", function(req, res) {
-
-        var movieTerm = req.params.movieterm;
-        console.log("Movie Search Term: " + movieTerm);
-
-        var queryUrl = "http://www.omdbapi.com/?s=" + movieTerm + "&y=&plot=short&apikey=" + OMDB_KEY;
-        console.log("omdb query url: " + queryUrl);
-        request(queryUrl, function(error, response, body) {
-            // If the request is successful
-            if (!error && response.statusCode === 200) {
-                console.log(body);
-                return res.json(JSON.parse(body));
-            }
-
-        });
-
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            client.games({
+                fields: '*',
+                limit: 10,
+                offset: 0,
+                order: 'popularity:desc',
+                search: searchTerm
+            }).then(response => {
+                // response.body contains the parsed JSON response to this query
+                console.log(response.body);
+                
+                return res.json(response.body);
     
-    // get movies based on games with date released
-    app.get("/api/movies/:gameterm/:date", function(req, res) {
-        var gameTerm = req.params.gameterm;
-        var date = req.params.date;
-        console.log("Game Search Term: " + gameTerm + "Date: " + date);
-
-    });
-
-    // get games based on movies with date released
-    app.get("/api/games/:movieterm/:date", function(req, res) {
-        var movieTerm = req.params.movieterm;
-        var date = req.params.date;
-        console.log("Movie Search Term: " + movieTerm + "Date: " + date);
+            }).catch(error => {
+                throw error;
+            });
+        }
 
     });
 
