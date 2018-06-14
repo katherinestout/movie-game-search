d894531ee15743a8044583fbbb8b1bde353d6f61
nchenari <nchenari@iMac.local>
2018-06-13 22:10:19 -0400

    fixed issue with server.js routing, tested igdb node wrapper

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-
+var request = require("request");
 var igdb = require("igdb-api-node").default;
 
 // keys
@@ -11,13 +11,14 @@ module.exports = function(app) {
     // get movies based on games
     app.get("/api/movies/:gameterm", function(req, res) {
         
-        var gameTerm = req.params.movieterm;
+        var gameTerm = req.params.gameterm;
         console.log("Game Search Term: " + gameTerm);
 
         // Run a request to the IGDB API with the title specified
         const client = igdb(IGDB_KEY);
+
         client.games({
-            fields: 'name',
+            fields: '*',
             limit: 5,
             offset: 0,
             order: 'name:desc',
@@ -36,24 +37,17 @@ module.exports = function(app) {
 
     // get games based on movies
     app.get("/api/games/:movieterm", function(req, res) {
+
+        var movieTerm = req.params.movieterm;
+        console.log("Movie Search Term: " + movieTerm);
+
         
-        var movieTerm = req.params.movieterm; 
-
-        // Run a request to the OMDB API with the title specified
-        var queryUrl = "http://www.omdbapi.com/?s=" + movieTerm + "&y=&plot=short&apikey=" + OMDB_KEY;
-        console.log(queryUrl);
-
-        request(queryUrl, function(error, response, body) {
-
-            // If the request is successful
-            if (!error && response.statusCode === 200) {
-                console.log(JSON.parse(body.Search));
-                return res.json(body.Search);
-                            
-            }
-        });
 
     });
+
+
+
+
 
     // get movies based on games with date released
     app.get("/api/movies/:gameterm/:date", function(req, res) {
