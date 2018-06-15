208e5968253e4c781d59498273b38beac77a0eff
nchenari <nimachenari@gmail.com>
2018-06-15 11:42:48 -0400

    finalized data returned for movie-list and game-list routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,59 +1,62 @@
-var axios = require("axios");
 var request = require("request");
 var igdb = require("igdb-api-node").default;
 
 // api keys
 const OMDB_KEY = "879503ae";
 const IGDB_KEY = "47a0d2b184712d0c3ae138e13c049062";
+const YOUTUBE_KEY = "AIzaSyAxaGmtGF-Ctvhj9OYggFV_cjiUXYJynSg";
 
 // routing
 module.exports = function(app) {
 
-    app.get("/api/search/:searchterm", function(req, res) {
+    app.get("/api/movie-list/:searchterm", function(req, res) {
         // *** SEARCH TERM MUST CONTAIN "-" BETWEEN WORDS (ex: "star-wars")
         var searchTerm = req.params.searchterm;
         console.log("search term: " + searchTerm);
 
-        // perfor multiple concurrent requests using axios
-        axios.all([getMovies(), getGames()])
-            .then(axios.spread(function (movieData, gameData) {
-                // return resulting data
-                console.log(movieData);
-                console.log(gameData);
-            }));
-
-        function getMovies() {
-            var queryUrl = "http://www.omdbapi.com/?s=" + searchTerm + "&y=&plot=short&apikey=" + OMDB_KEY;
+        // movies
+        var queryUrl = "http://www.omdbapi.com/?s=" + searchTerm + "&y=&plot=short&apikey=" + OMDB_KEY;
             console.log("omdb query url: " + queryUrl);
             request(queryUrl, function(error, response, body) {
                 // If the request is successful
                 if (!error && response.statusCode === 200) {
                     console.log(body);
-                    return res.json(JSON.parse(body));
+                    res.json(JSON.parse(body));
                 }
             });
-        }
-        
-        function getGames() {
-            const client = igdb(IGDB_KEY);
+    });
 
-            client.games({
-                fields: '*',
-                limit: 10,
-                offset: 0,
-                order: 'popularity:desc',
-                search: searchTerm
-            }).then(response => {
-                // response.body contains the parsed JSON response to this query
-                console.log(response.body);
-                
-                return res.json(response.body);
-    
-            }).catch(error => {
-                throw error;
-            });
-        }
+    app.get("/api/game-list/:searchterm", function(req, res) {
+        // *** SEARCH TERM MUST CONTAIN "-" BETWEEN WORDS (ex: "star-wars")
+        var searchTerm = req.params.searchterm;
+        console.log("search term: " + searchTerm);
+
+        // games   
+        const client = igdb(IGDB_KEY);
+
+        client.games({
+            fields: '*',
+            limit: 10,
+            offset: 0,
+            order: 'popularity:desc',
+            search: searchTerm
+        }).then(response => {
+            // response.body contains the parsed JSON response to this query
+            console.log(response.body);
+            res.json(response.body);
+
+        }).catch(error => {
+            throw error;
+        });
+    });
+
+    app.get("/api/movie/:title", function(req, res) {
 
     });
 
-}
+    app.get("api/game/:title", function(req, res) {
+
+    });
+};
+
+
